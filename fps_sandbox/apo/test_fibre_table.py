5a8b346d3ce5c2084a12018510435dc8c2eeaa03
José Sánchez-Gallego <gallegoj@uw.edu>
2022-01-13 09:52:49 -0800

    Stub for test_fibre_table.py

---
+++ fps_sandbox/apo/test_fibre_table.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2022-01-13
+# @Filename: test_fibre_table.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+
+def test_fibre_table(design_id: int, epoch: float):
+    return
+
+
+if __name__ == "__main__":
+    test_fibre_table(35891, 2459592.5733)
