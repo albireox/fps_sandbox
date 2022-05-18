994790810561cb6e22fedbcb70579333a5297986
José Sánchez-Gallego <gallegoj@uw.edu>
2022-05-17 17:49:24 -0700

    Update check_robot_dithers.py

--- fps_sandbox/apo/check_robot_dithers.py
+++ fps_sandbox/apo/check_robot_dithers.py
@@ -9,41 +9,197 @@
 import os
 import pathlib
 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy
 import pandas
+import seaborn
 
-from fps_sandbox.apo.check_confSummaryF import read_confSummary
+from jaeger.target.tools import read_confSummary
 
 
+matplotlib.use("MacOSX")
+seaborn.set_theme()
+
+
+RESULTS = pathlib.Path(__file__).parent / "../results/dither_distribution"
 SDSSCORE_DIR = pathlib.Path(os.environ["SDSSCORE_DIR"]) / "apo" / "summary_files"
 
 
-def check_robot_dither_distribution():
+def check_robot_dither_distribution(
+    parent_id: int,
+    dither_id: int,
+    flavour_parent: str = "",
+    flavour_dither: str = "",
+):
 
-    CONFIGURATION_IDS = [5111, 5112, 5113, 5114, 5093, 5092]
+    RESULTS.mkdir(exist_ok=True, parents=True)
 
-    ys = []
-    for cid in CONFIGURATION_IDS:
-        for f in [True, False]:
-            cid_path_xx = SDSSCORE_DIR / f"{int(cid / 100):04d}XX"
-            cid_path = cid_path_xx / f"confSummary{'F' if f else ''}-{cid}.par"
-            # if not cid_path.exists():
-            #     continue
+    header, parent = read_confSummary(parent_id, flavour=flavour_parent)
+    header_d, dither = read_confSummary(dither_id, flavour=flavour_dither)
 
-            header, df = read_confSummary(cid_path)
+    radius = header_d["dither_radius"] * 1000.0
 
-            df.loc[:, "isFVC"] = int(f)
-            df.loc[:, "configurationId"] = int(header["configuration_id"])
-            df.loc[:, "parent_configuration"] = float(header["parent_configuration"])
-            df.loc[:, "dither_radius"] = float(header["dither_radius"])
+    wok_delta = dither.loc[:, ["xwok", "ywok"]] - parent.loc[:, ["xwok", "ywok"]]
+    wok_delta *= 1000.0
 
-            ys.append(df)
+    met = wok_delta.loc[pandas.IndexSlice[:, "METROLOGY"], :]
 
-    data = pandas.concat(ys)
-    data.reset_index(inplace=True)
-    data.set_index(["configurationId", "positionerId", "fiberType"], inplace=True)
+    xwok_perc = numpy.round((numpy.abs(met.xwok) <= radius).sum() / len(met) * 100.0, 2)
+    ywok_perc = numpy.round((numpy.abs(met.ywok) <= radius).sum() / len(met) * 100.0, 2)
 
-    print(data)
+    g = seaborn.jointplot(
+        x="xwok",
+        y="ywok",
+        data=met,
+        height=10,
+        xlim=(-200, 200),
+        ylim=(-200, 200),
+    )
+    g.plot_joint(seaborn.kdeplot, color="g", zorder=10, levels=8)
+    g.plot_marginals(seaborn.histplot, kde=True)
+
+    g.ax_joint.text(
+        -190,
+        185,
+        f"{parent_id}{'(F)' if flavour_parent else ''} vs "
+        f"{dither_id}{'(F)' if flavour_dither else ''}",
+        size="large",
+    )
+    g.ax_joint.text(
+        -190,
+        -190,
+        f"Design {header['design_id']}. "
+        + ((f"Dither {radius} " + r"$\mu{\rm m}$.") if radius > -999.0 else ""),
+        size="large",
+    )
+
+    if radius > -999.0:
+        g.ax_joint.text(
+            190,
+            185,
+            r"$x_{\%}=" + f"{xwok_perc}" + r"\% \quad $"
+            r"$y_{\%}=" + f"{ywok_perc}" + r"\%$",
+            ha="right",
+            size="large",
+        )
+
+    g.ax_joint.set_xlabel(r"$\Delta x_{\rm wok}$")
+    g.ax_joint.set_ylabel(r"$\Delta y_{\rm wok}$")
+
+    g.fig.savefig(
+        RESULTS
+        / f"dither_{parent_id}{flavour_parent}_{dither_id}{flavour_dither}_wok.pdf"
+    )
+
+    # cos_dec = numpy.cos(numpy.deg2rad(header["decCen"]))
+    # sky_delta = dither.loc[:, ["ra", "dec"]] - parent.loc[:, ["ra", "dec"]]
+    # sky_delta.ra *= cos_dec
+    # sky_delta *= 3600.0
+
+    # g = seaborn.jointplot(
+    #     x="ra",
+    #     y="dec",
+    #     data=sky_delta,
+    #     hue="fiberType",
+    #     height=10,
+    #     xlim=(-3, 3),
+    #     ylim=(-3, 3),
+    # )
+    # g.plot_joint(seaborn.kdeplot, color="r", zorder=10, levels=8)
+
+    # g.ax_joint.text(
+    #     -2.8,
+    #     2.8,
+    #     f"{parent_id}{'(F)' if flavour_parent else ''} vs "
+    #     f"{dither_id}{'(F)' if flavour_dither else ''}",
+    #     size="large",
+    # )
+    # g.ax_joint.text(
+    #     -2.8,
+    #     -2.8,
+    #     f"Design {header['design_id']}. Dither {header_d['dither_radius']}.",
+    #     size="large",
+    # )
+
+    # g.fig.savefig(
+    #     RESULTS
+    #     / f"dither_{parent_id}{flavour_parent}_{dither_id}{flavour_dither}_sky.pdf"
+    # )
+
+    plt.close("all")
+
+
+def check_fibre_separation(parent_id: int, dither_id: int):
+
+    for config_id in [parent_id, dither_id]:
+        _, data = read_confSummary(config_id)
+
+        met = data.loc[(slice(None), "METROLOGY"), :].reset_index(1)
+        boss = data.loc[(slice(None), "BOSS"), :].reset_index(1)
+        apog = data.loc[(slice(None), "APOGEE"), :].reset_index(1)
+
+        met["dboss"] = numpy.hypot(met.xwok - boss.xwok, met.ywok - boss.ywok)
+        met["dapog"] = numpy.hypot(met.xwok - apog.xwok, met.ywok - apog.ywok)
+
+        print()
+        print(f"Configuration {config_id}")
+        print("dboss")
+        print(met.dboss.describe())
+        print("dapog")
+        print(met.dapog.describe())
+
+    _, parent = read_confSummary(parent_id)
+    pmet = parent.loc[(slice(None), "METROLOGY"), :].reset_index(1)
+    pboss = parent.loc[(slice(None), "BOSS"), :].reset_index(1)
+    papog = parent.loc[(slice(None), "APOGEE"), :].reset_index(1)
+
+    _, dither = read_confSummary(dither_id)
+    dmet = dither.loc[(slice(None), "METROLOGY"), :].reset_index(1)
+    dboss = dither.loc[(slice(None), "BOSS"), :].reset_index(1)
+    dapog = dither.loc[(slice(None), "APOGEE"), :].reset_index(1)
+
+    pmet["delta_dboss"] = (
+        numpy.hypot(
+            (pmet.xwok - dmet.xwok) - (pboss.xwok - dboss.xwok),
+            (pmet.ywok - dmet.ywok) - (pboss.ywok - dboss.ywok),
+        )
+        * 1000
+    )
+    pmet["delta_dapog"] = (
+        numpy.hypot(
+            (pmet.xwok - dmet.xwok) - (papog.xwok - dapog.xwok),
+            (pmet.ywok - dmet.ywok) - (papog.ywok - dapog.ywok),
+        )
+        * 1000.0
+    )
+
+    breakpoint()
 
 
 if __name__ == "__main__":
-    check_robot_dither_distribution()
+
+    # check_fibre_separation(5203, 5204)
+
+    for parent_id in range(5203, 5222, 2):
+        dither_id = parent_id + 1
+        check_robot_dither_distribution(
+            parent_id,
+            parent_id,
+            "",
+            "F",
+        )
+        check_robot_dither_distribution(
+            dither_id,
+            dither_id,
+            "",
+            "F",
+        )
+        for flavour_parent in ["", "F"]:
+            for flavour_dither in ["", "F"]:
+                check_robot_dither_distribution(
+                    parent_id,
+                    dither_id,
+                    flavour_parent,
+                    flavour_dither,
+                )
