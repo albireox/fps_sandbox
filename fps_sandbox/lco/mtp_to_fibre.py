c36c496f04bbffd449691d7d17dc98db5b554be1
José Sánchez-Gallego <gallegoj@uw.edu>
2022-08-25 16:31:50 -0700

    Add scripts to create MTP mapping at LCO

---
+++ fps_sandbox/lco/mtp_to_fibre.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2022-08-25
+# @Filename: mtp_to_fibre.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+import itertools
+import pathlib
+
+import pandas
+
+from coordio import calibration
+
+
+def mtp_to_fibre_lco():
+    """Returns a table of MTP and MTP block to spectrograph fibre ID, for LCO."""
+
+    mtp_block_ids = list(itertools.product("ABCDE", "123456"))
+
+    fiber_id = 1
+
+    assignments = []
+    for mtp in range(1, 11):
+        for ll, nn in mtp_block_ids[::-1]:
+            assignments.append((mtp, ll + nn, fiber_id))
+            fiber_id += 1
+
+    data = pandas.DataFrame(
+        assignments,
+        columns=["LongLinkMTP", "MTPFiber", "APOGEEFiber"],
+    )
+
+    return data
+
+
+def update_fiberAssignments(file_: pathlib.Path | str | None = None):
+
+    if file_ is None:
+        fa = calibration.fiberAssignments.copy().reset_index()
+    else:
+        fa = pandas.read_csv(str(file_))
+
+    mtp = mtp_to_fibre_lco()
+
+    for _, (mtp_id, mtp_fibre, apogee_fibre) in mtp.iterrows():
+        fa.loc[
+            (fa.LongLinkMTP == mtp_id) & (fa.MTPFiber == mtp_fibre), "APOGEEFiber"
+        ] = apogee_fibre
+
+    return fa
